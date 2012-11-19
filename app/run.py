64e16b409919f96c4d4b7a471d1121635fc43240
Julien Bouquillon <julien@bouquillon.com>
2012-11-19 20:38:44 +0100

    use sqlite DB

--- app/run.py
+++ app/run.py
@@ -1,45 +1,59 @@
 # -*- encoding: UTF-8 -*-
 
 import os
+import shutil
 import json
-import random
 import bottle
 from bottle import route, run, static_file, redirect, request
-
+import sqlite3
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 
-QUESTIONS_FILE = 'questions.json'
+SAMPLE_DB = 'sample.sqlite'
+QUESTIONS_DB = 'questions.sqlite'
+
+
+def db_dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0].lower()] = row[idx]
+    return d
+
+
+def db_get_cursor():
+    in_db = os.path.join(BASE_DIR, QUESTIONS_DB)
+    if not os.path.isfile(in_db):
+        shutil.copyfile(os.path.join(BASE_DIR, SAMPLE_DB), in_db)
+    conn = sqlite3.connect(in_db)
+    conn.row_factory = db_dict_factory
+    conn.isolation_level = None
+    c = conn.cursor()
+    return c
+
+
+def db_exec(sql, sqlargs=None):
+    c = db_get_cursor()
+    if sqlargs:
+        c.execute(sql, sqlargs)
+    else:
+        c.execute(sql)
+    return c
 
 
 def get_questions():
-    try:
-        questions = json.load(open(QUESTIONS_FILE, 'r'))
-    except:
-        questions = []
-    return questions
+    questions = db_exec('SELECT * FROM questions')
+    return questions.fetchall()
 
 
-def save_questions(questions):
-    json.dump(questions, open(QUESTIONS_FILE, 'w'))
+def get_question(id):
+    questions = db_exec('SELECT * FROM questions WHERE ID=?', (id, ))
+    return questions.fetchone()
 
 
-def add_question(text, theme='question'):
-    q = get_questions()
-    question = {
-        'id': get_random_id(),
-        'text': text,
-        'theme': theme,
-        'done': False
-    }
-    q.append(question)
-    save_questions(q)
-    return question
-
-
-def get_random_id():
-    return random.randint(100000, 99999999999)
+def add_question(text="test", theme='question'):
+    c = db_exec("INSERT INTO questions (text, theme) VALUES(?, ?)", (text, theme))
+    return get_question(c.lastrowid)
 
 
 # routes
@@ -77,30 +91,25 @@ def statics(filepath):
 def questions():
     if request.method == 'POST':
         data = request.json
-        return add_question(data.get('text', ''), data.get('theme', ''))
-    return static_file('questions.json', root=BASE_DIR)
+        return json.dumps(add_question(data.get('text', ''), data.get('theme', '')))
+    return json.dumps(get_questions())
 
 
 @route('/questions.json/<id:int>', method="ANY")
 def questions2(id):
-    questions = get_questions()
     res = None
-    for idx, q in enumerate(questions):
-        if q.get('id') == id:
-            if request.method == 'DELETE':
-                questions.pop(idx)
-            elif request.method == 'GET':
-                res = q
-            elif request.method == 'POST':
-                data = request.json
-                q['text'] = data.get('text')
-                q['done'] = data.get('done', False)
-                res = q
-    save_questions(questions)
-            #print "REMOVE", id
-        #data = request.json
-        #add_question(data.get('text', ''))
-    return res
+    if request.method == 'DELETE':
+        # delete a questions
+        db_exec("DELETE FROM QUESTIONS WHERE ID=?", (id, ))
+    elif request.method == 'GET':
+        # get single question
+        res = get_question(id)
+    elif request.method == 'POST':
+        # update a question
+        data = request.json
+        db_exec("UPDATE questions SET text=?, done=? WHERE ID=?", (data.get('text'), data.get('done'), id))
+        res = get_question(id)
+    return json.dumps(res)
 
 
 bottle.debug(True)
